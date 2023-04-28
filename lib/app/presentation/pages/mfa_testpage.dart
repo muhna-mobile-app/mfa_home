648c992db65378112698bd1109e582f593208aab
kaio-nink <51462685+kaio-nink@users.noreply.github.com>
2023-04-28 09:42:48 -0300

    Initial commit

---
+++ lib/app/presentation/pages/mfa_testpage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TemplatePate extends StatelessWidget {
+  const TemplatePate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
